10e3844f318f6fc0c1ebf18c4c868ab6fe3d35a5
PixelVam <vamdev26@gmail.com>
2026-04-26 20:59:27 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,107 @@
+import time
+
+import streamlit as st
+
+from resources import data_ncert_8_mathc1
+
+st.title("FiguraSolve")
+st.markdown("The Perfect Place For Your Learning,Creativity and Thinking")
+st.divider()
+a = st.selectbox(
+    "Please Select a Board of Education",
+    ["Default(Toggle)", "AI-ResourceBuilder(RB)[Upcoming]", "CBSE", "STATE EDUCATION"],
+)
+if a == "CBSE":
+    classn = st.selectbox(
+        "Please select your Class", ["Default(Toggle)", "Class 8th", "Class 9th"]
+    )
+    st.divider()
+    if classn == "Class 8th":
+        with st.spinner("Loading Class 8th NCERT Resources", show_time=True):
+            time.sleep(5)
+            st.subheader(f"Resources for Class 8th,({a})")
+            st.markdown(
+                "Curated and Perfected Resources for Class 8th Ncert.Check all The Free Resources"
+            )
+            st.divider()
+            subjectsn = st.selectbox(
+                "Please Select a Subject(NCERT)",
+                [
+                    "Default(Toggle)",
+                    "Maths(Part 1)",
+                    "Science",
+                    "English",
+                    "Maths(Part 2)",
+                ],
+            )
+            st.divider()
+            chaptersn = st.selectbox(
+                "Please Select a Chapter",
+                ["Default(Toggle)", "Chapter 1", "Chapter 2", "Chapter 3"],
+            )
+            if subjectsn == "Maths(Part 1)" and chaptersn == "Chapter 1":
+                st.header("Ganita Prakash(Part 1) Chapter 1: A Square and A Cube")
+                st.divider()
+                linktextbook1 = data_ncert_8_mathc1["Chapter 1"]["Resources"][
+                    "Maths Textbook1"
+                ]
+                st.subheader("Math Textbooks Part 1 and 2")
+                st.divider()
+                st.markdown(f"1. [Math Textbook[Part 1]]({linktextbook1})")
+                linktextbook2 = data_ncert_8_mathc1["Chapter 1"]["Resources"][
+                    "Maths Textbook2"
+                ]
+                st.divider()
+                st.markdown(f"2. [Math Textbook[Part 2]]({linktextbook2})")
+                st.divider()
+                st.markdown("A Square and A Cube(Contents)")
+                with st.expander("1. Link Resources"):
+                    linkti = data_ncert_8_mathc1["Chapter 1"]["Resources"][
+                        "Link Resources"
+                    ]["l1"]
+                    st.markdown(f"[Tiwari Academy]({linkti})")
+                    linkti1 = data_ncert_8_mathc1["Chapter 1"]["Resources"][
+                        "Link Resources"
+                    ]["l2"]
+                    st.markdown(f"[Ncert.Pdf]({linkti1})")
+                    linkti2 = data_ncert_8_mathc1["Chapter 1"]["Resources"][
+                        "Link Resources"
+                    ]["l3"]
+                    st.markdown(f"[Cbse.Academic]({linkti2})")
+                    linkti3 = data_ncert_8_mathc1["Chapter 1"]["Resources"][
+                        "Link Resources"
+                    ]["l4"]
+                    st.markdown(f"[MathisFun]({linkti3})")
+                    st.divider()
+                with st.expander("2. Video Resources"):
+                    st.divider()
+                    vidti1 = data_ncert_8_mathc1["Chapter 1"]["Resources"][
+                        "Link Resources"
+                    ]["Video Resources"]["vid1"]
+                    st.markdown(f"[Youtube Video(1)]({vidti1})")
+                    vidti2 = data_ncert_8_mathc1["Chapter 1"]["Resources"][
+                        "Link Resources"
+                    ]["Video Resources"]["vid2"]
+                    st.markdown(f"[Youtube Video(2)]({vidti2})")
+                    vidti3 = data_ncert_8_mathc1["Chapter 1"]["Resources"][
+                        "Link Resources"
+                    ]["Video Resources"]["vid3"]
+                    st.markdown(f"[Youtube Video(3)]({vidti3})")
+                    vidti4 = data_ncert_8_mathc1["Chapter 1"]["Resources"][
+                        "Link Resources"
+                    ]["Video Resources"]["vid4"]
+                    st.markdown(f"[Youtube Video(4)]({vidti4})")
+                with st.expander("3.Phet and Interaction Resources"):
+                    ph1 = data_ncert_8_mathc1["Chapter 1"]["Resources"][
+                        "Link Resources"
+                    ]["Phet Simulation"]["phet_simulation_1"]
+                    st.markdown(f"[Algebra Model by Phet Simulations]({ph1})")
+                    inter1 = data_ncert_8_mathc1["Chapter 1"]["Resources"][
+                        "Link Resources"
+                    ]["Phet Simulation"]["Interaction"]["I1"]
+                    st.markdown(f"[TCSION Interaction]({inter1})")
+            if subjectsn == "Maths(Part 1)" and chaptersn == "Chapter 2":
+                st.title("Ganita Prakash|Chapter-2:Power Play")
+                st.subheader("Resources For Ganita Prakash|Power Play")
+                st.divider()
+                st.markdown("")
---
+++ resources.py
@@ -0,0 +1,41 @@
+data_ncert_8_mathc1 = {
+    "Chapter 1": {
+        "Resources": {
+            "Maths Textbook1": "https://www.scribd.com/document/892880323/Ganita-prakash-class-8-Maths-book",
+            "Maths Textbook2": "https://ncert.nic.in/textbook.php?hegp2=0-7",
+            "Link Resources": {
+                "l1": "https://www.tiwariacademy.com/ncert-solutions/class-8/maths/",
+                "l2": "https://ncert.nic.in/textbook/pdf/hegp101.pdf",
+                "l3": "https://cbseacademic.nic.in/curriculum_2026.html",
+                "l4": "https://www.mathsisfun.com/numbers/cube-root.html",
+                "l6": "",
+                "Video Resources": {
+                    "vid1": "https://www.youtube.com/watch?v=_ffqUqyUzmg&t=1373s",
+                    "vid2": "https://www.youtube.com/watch?v=q3sfsvPLidc&list=PLM7yq3heQr2O2Cl4FZHN3neJg4A5fKKtN",
+                    "vid3": "https://www.youtube.com/watch?v=C3yM4cZoM1E",
+                    "vid4": "https://www.youtube.com/watch?v=jsz4Sa_2C_g",
+                },
+                "Phet Simulation": {
+                    "phet_simulation_1": "https://phet.colorado.edu/en/simulations/area-model-algebra",
+                    "Interaction": {
+                        "I1": "https://www.tcsion.com/ncert/class8-maths-a-square-and-a-cube",
+                    },
+                },
+            },
+        }
+    }
+}
+
+data_ncert_8_mathc2 = {
+    "Resources(Link)": {
+        "l1": "https://ncert.nic.in/textbook/pdf/hegp102.pdf",
+        "l2": "https://www.learncbse.in/power-play-class-8-solutions/",
+        "l3": "https://www.thestudypath.com/ncert-solutions-class-8-maths-ganita-prakash-part-1-chapter-2/",
+        "l4": "https://www.tiwariacademy.com/ncert-solutions/class-8/maths/ganita-prakash-chapter-2/",
+        "Resources(Vid)": {
+            "Resources(I)": {
+
+            }
+        }
+    }
+}
